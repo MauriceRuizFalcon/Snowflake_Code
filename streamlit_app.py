426176a266c1ded312199991b5b48818eb295d8e
MauriceRuizFalcon <72233613+MauriceRuizFalcon@users.noreply.github.com>
2022-05-12 11:50:24 -0400

    Update streamlit_app.py
    se cambia opcion de lista para seleccionar por nombre de fruta

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,7 @@ streamlit.text('🐔 Pollo en papas')
 streamlit.text('🥑 Ensalada')
 streamlit.header('🍌🥭 Crea tus batidos de frutas 🥝🍇')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 # Se coloca una seleccion de la lista de frutas
 streamlit.multiselect("Selecciona tus frutas favoritas :", list(my_fruit_list.index))
 # despliega la tabla en la pagina
