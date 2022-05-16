abb3066ff2f38f021b8aedd1b3d7806d98609e91
MauriceRuizFalcon <72233613+MauriceRuizFalcon@users.noreply.github.com>
2022-05-16 15:15:01 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 streamlit.title('Restaurante Mayimba')
 streamlit.header('MENÚ DESAYUNO')
-streamlit.text('Cafe de olla')
+streamlit.header('Cafe de olla')
 streamlit.text('Huevos rancheros')
 streamlit.text('🍞 Pan')
 streamlit.text('Pan de dulce')
@@ -15,12 +15,10 @@ streamlit.header('🍌🥭 Crea tus batidos de frutas 🥝🍇')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 # Se coloca una seleccion de la lista de frutas
-
 fruits_selected = streamlit.multiselect("Selecciona tus frutas favoritas :", list(my_fruit_list.index),['Avocado','Strawberries','Banana'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 # despliega la tabla en la pagina
 streamlit.dataframe(fruits_to_show)
-
 # new section to display fruityvice api response
 # import requests
 # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
